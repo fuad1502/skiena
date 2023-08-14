978ea159f9f1707c8673381e611eaffc83c63d3d
Fuad Ismail <fuad1502@gmail.com>
2023-08-14 17:38:15 +0700

    Solved chapter 3 problem 15

--- chapter3/3-15/transform-balanced-tree-testsuit.cpp
+++ chapter3/3-15/transform-balanced-tree-testsuit.cpp
@@ -1,6 +1,18 @@
 #include <gtest/gtest.h>
+#include "transform-balanced-tree.hpp"
 
 TEST(TransformBalancedTree, All)
 {
-
+  DictTree<int> dict;
+  dict.insert(1);
+  dict.insert(2);
+  dict.insert(3);
+  dict.insert(4);
+  dict.insert(5);
+  std::cout << "Before balanced: " << std::endl;
+  std::cout << dict;
+  pTree<int> balancedTree = toBalancedTree(dict.data());
+  int unused1, unused2;
+  std::cout << "After balanced: " << std::endl;
+  std::cout << treeString(balancedTree, unused1, unused2);
 }
--- chapter3/3-15/transform-balanced-tree.hpp
+++ chapter3/3-15/transform-balanced-tree.hpp
@@ -1,2 +1,79 @@
 #pragma once
 #include "cli-tree.hpp"
+
+template<typename T>
+pTree<T> toBalancedTree(pTree<T> tree)
+{
+  // manually create balanced tree of length n
+  int n = getNumberOfNodes(tree);
+  pTree<T> balancedTree = std::shared_ptr<Tree<T>>(new Tree<T>);
+  createBalancedChildTrees(balancedTree, n - 1);
+  // create tree list of the balanced tree
+  pList<pTree<T>> balancedListHead, balancedListTail;
+  createListFromTree(balancedTree, balancedListHead, balancedListTail);
+  // create tree list of the original tree
+  pList<pTree<T>> listHead, listTail;
+  createListFromTree(tree, listHead, listTail);
+  // copy data from original to balanced tree
+  pList<pTree<T>> node = listHead;
+  pList<pTree<T>> balancedList = balancedListHead;
+  while(node != nullptr) {
+    balancedList->data->data = node->data->data;
+    node = node->next;
+    balancedList = balancedList->next;
+  }
+  // return balanced tree
+  return balancedTree;
+}
+
+template<typename T>
+void createBalancedChildTrees(pTree<T> tree, int n)
+{
+  if(n == 0) {
+    ;
+  } else if(n == 1) {
+    tree->left = std::shared_ptr<Tree<T>>(new Tree<T>);
+  } else {
+    tree->left = std::shared_ptr<Tree<T>>(new Tree<T>);
+    tree->right = std::shared_ptr<Tree<T>>(new Tree<T>);
+    // create half (round up) of the remaining balanced trees to the left
+    createBalancedChildTrees(tree->left, (n - 2 + 1) / 2);
+    // create half (round down) of the remaining balanced trees to the right
+    createBalancedChildTrees(tree->right, (n - 2) / 2);
+  }
+}
+
+template<typename T>
+void createListFromTree(pTree<T> tree, pList<pTree<T>>& head, pList<pTree<T>>& tail)
+{
+  if(tree == nullptr) {
+    head = nullptr;
+    tail = nullptr;
+  } else {
+    // recurse left
+    pList<pTree<T>> leftHead, leftTail;
+    createListFromTree(tree->left, leftHead, leftTail);
+    // create node
+    pList<pTree<T>> node = std::shared_ptr<List<pTree<T>>>(new List<pTree<T>>);
+    node->data = tree;
+    // recurse right
+    pList<pTree<T>> rightHead, rightTail;
+    createListFromTree(tree->right, rightHead, rightTail);
+    // Connect node with left recursion
+    if(leftTail != nullptr) {
+      leftTail->next = node;
+      node->previous = leftTail;
+      head = leftHead;
+    } else {
+      head = node;
+    }
+    // Connect node with right recursion
+    if(rightHead != nullptr) {
+      node->next = rightHead;
+      rightHead->previous = node;
+      tail = rightTail;
+    } else {
+      tail = node;
+    }
+  }
+}
