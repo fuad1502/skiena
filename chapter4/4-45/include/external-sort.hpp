0c975e3b8a10b8074fec6598e35903da085ba2cd
Fuad Ismail <fuad1502@gmail.com>
2023-09-21 11:20:37 +0700

    Created problem 45 chapter 4 project structure and main function prototype

---
+++ chapter4/4-45/include/external-sort.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <cstddef>
+#include <string>
+
+namespace ExternalSort {
+void sortText(std::string inputFileName, std::string outputFileName, size_t workingMemoryInBytes);
+}
